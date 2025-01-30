1b415a76cbf207270ea6411419d4be57871102f4
tam210 <ossandon.t20@gmail.com>
2025-01-30 20:27:45 -0300

    feature: configuraciion para depuracion

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./config/database'); // Tu configuración de Sequelize
 
 // Rutas
-const setRouter= require('./routes/sets');
+const setRouter = require('./routes/sets');
 const cardRouter = require('./routes/cards');
 
-const sequelize = require('./config/database');
 const app = express();
 const PORT = 3001;
+const MODE = process.env.NEXT_PUBLIC_MODE || "INDEFINIDO";
 
 
-
-app.use(cors({ //cambiar a frontend
-  origin: 'http://localhost:3000', // Asegúrate de que este sea el nombre del servicio frontend en Docker Compose
+// Configurar CORS
+app.use(cors({
+  origin: `http://${MODE}:3000`, // Nombre del servicio frontend en Docker Compose
   credentials: true,
 }));
 
@@ -23,12 +24,28 @@ app.use(express.json());
 app.use('/api/sets', setRouter);
 app.use('/api/cards', cardRouter);
 
-// Iniciar el servidor
-sequelize.sync({ alter: true }).then(() => {
-  console.log("Modelos sincronizados correctamente.");
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  });
-}).catch(err => {
-  console.error("Error de sincronización:", err);
-});
+// Función de reintento de conexión a la base de datos
+async function connectWithRetry() {
+  try {
+    await sequelize.authenticate(); // Intentar la conexión a la base de datos
+    console.log("Conexión a la base de datos establecida con éxito.");
+    
+    // Sincronizar los modelos con la base de datos
+    await sequelize.sync({ alter: true });
+    console.log("Modelos sincronizados correctamente.");
+    
+
+
+    
+    // Iniciar el servidor después de la conexión exitosa
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://${MODE}:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error al conectar a la base de datos, reintentando...", error);
+    setTimeout(connectWithRetry, 5000); // Reintentar cada 5 segundos
+  }
+}
+
+// Intentar conectar y reiniciar el proceso
+connectWithRetry();
